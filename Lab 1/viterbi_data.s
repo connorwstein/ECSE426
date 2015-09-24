5b9edeae90c32ab6aea568ac8b2b6294ef002006
connorwstein <connor.stein@mail.mcgill.ca>
2015-09-24 15:43:17 -0700

    added data file

---
+++ Lab 1/viterbi_data.s
@@ -0,0 +1,14 @@
+	AREA data, DATA, READWRITE
+	export s
+	export v
+	export transition
+	export emission
+	export prior
+	export observation
+s DCD 1
+v DCD 1
+transition DCD 1,2,3,4
+emission DCD 1,2,3,4,5,6
+prior DCD 1,2
+observation DCD 1,2,3,4,5,6
+	END
