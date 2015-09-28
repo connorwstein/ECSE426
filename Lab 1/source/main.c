012b99dfd3006a906ddf7c2562b28af6039c7997
TakeshiMusgrave <takeshi.musgrave@mail.mcgill.ca>
2015-09-28 18:48:32 -0400

    The C version works with 2d arrays now

--- Lab 1/source/main.c
+++ Lab 1/source/main.c
@@ -12,63 +12,55 @@ typedef struct {
 	float prior[S_DEF];
 }hmm_desc;
 
-//int ViterbiUpdate_C(float* InputArray, float* OutputArray, hmm_desc* hmm, int Observation){
+int ViterbiUpdate_C(float* InputArray, float* OutputArray, hmm_desc* hmm, int Observation){
 
-//	for(int s = 0; s<hmm->S; s++)
-//	{
-//		float trans_p[hmm->S];
-//		for(int i = 0; i<hmm->S; i++)
-//		{
-//			//printf("%f, %f\n",InputArray[i],hmm->transition[i][s]);
-//			//printf("multipilcation test %f\n",InputArray[i]*(hmm->transition[i][s]));
-//			trans_p[i] = InputArray[i]*(hmm->transition[i*hmm->S + s]);
-//			//printf("%f\n",trans_p[i]);
-//		}
-//		
-//		float maximumValue = -1;
-//		float maximumValueIndex = -1;
-//		
-//		for(int i = 0; i<hmm->S; i++)
-//		{
-//			if(trans_p[i] > maximumValue)
-//			{
-//				maximumValue = trans_p[i];
-//				maximumValueIndex = i;
-//			}
-//		}
-//		
-//		//vit at time t
-//		OutputArray[s] = maximumValue;
-//		
-//		//printf("viterbiOut[%d] = %f\n",s,OutputArray[s]);
-//		
-//		//psi at time t
-//		OutputArray[hmm->S + s] = maximumValueIndex;
-//		
-//		//printf("psiOut[%d] = %f\n",s,OutputArray[hmm->S + s]);
-//		
-//		OutputArray[s] *= hmm->emission[s*hmm->V + Observation];
-//		
-//	} 
-//	
-//	float sumOfVitOutput = 0;
-//	
-//	for(int i = 0; i<hmm->S; i++)
-//	{
-//		sumOfVitOutput += OutputArray[i];
-//	}
-//	
-//	float scalingFactor = 1.0f/sumOfVitOutput;
-//	
-//	for(int i = 0; i<hmm->S; i++)
-//	{
-//		OutputArray[i] *= scalingFactor;
-//		//printf("Output[%d] = %f\n",i,OutputArray[i]);
-//	}	
-//	
-//	return 0;
-//	
-//}
+	for(int s = 0; s<hmm->S; s++)
+	{
+		float trans_p[hmm->S];
+		for(int i = 0; i<hmm->S; i++)
+		{
+			trans_p[i] = InputArray[i]*(hmm->transition[i][s]);
+		}
+		
+		float maximumValue = -1;
+		float maximumValueIndex = -1;
+		
+		for(int i = 0; i<hmm->S; i++)
+		{
+			if(trans_p[i] > maximumValue)
+			{
+				maximumValue = trans_p[i];
+				maximumValueIndex = i;
+			}
+		}
+		
+		//vit at time t
+		OutputArray[s] = maximumValue;
+		
+		//psi at time t
+		OutputArray[hmm->S + s] = maximumValueIndex;
+		
+		OutputArray[s] *= hmm->emission[s][Observation];
+		
+	} 
+	
+	float sumOfVitOutput = 0;
+	
+	for(int i = 0; i<hmm->S; i++)
+	{
+		sumOfVitOutput += OutputArray[i];
+	}
+	
+	float scalingFactor = 1.0f/sumOfVitOutput;
+	
+	for(int i = 0; i<hmm->S; i++)
+	{
+		OutputArray[i] *= scalingFactor;
+	}	
+	
+	return 0;
+	
+}
 
 
 extern void ViterbiUpdate_asm(float* InputArray, float* OutputArray, int Observation, hmm_desc* hmm);
@@ -87,6 +79,8 @@ int main()
 	
 	float vitInput[S_DEF];
 	float vitpsiOutput[2*S_DEF];
+		
+	memset(vitpsiOutput, 0, sizeof(vitpsiOutput));
 	
 	int numberOfTimeSteps = 6;
 						
@@ -105,16 +99,12 @@ int main()
 		printf("vitInput[%d] = %f",i,vitInput[i]);
 	}
 	printf("\n");
-	
-	//float vitMatrix[hmm.S][hmm.V-1];
-	//memset(vitMatrix, 0, sizeof(vitMatrix));
-	
-	memset(vitpsiOutput, 0, sizeof(vitpsiOutput));
+
 	
 	for(int t=1;t<numberOfTimeSteps;t++)
 	{
 		
-		//ViterbiUpdate_C(vitInput, vitpsiOutput, &hmm, 0);
+		//ViterbiUpdate_C(vitInput, vitpsiOutput, &hmm, observation[t]);
 		ViterbiUpdate_asm(vitInput,vitpsiOutput,observation[t],&hmm);
 		printf("timestep %d: vitInput is\n",t);
 		
@@ -126,16 +116,6 @@ int main()
 		}
 		printf("\n");
 	}
-
-//	ViterbiUpdate_asm(vitInput,vitpsiOutput,1,&hmm);
-//	
-//	for(int i=0;i<hmm.S;i++)
-//	{
-//		vitInput[i] = vitpsiOutput[i];
-//		
-//		printf("%f ",vitInput[i]);
-//	}
-//	printf("\n");
 	
 
 	return 0;
