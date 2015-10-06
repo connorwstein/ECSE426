a8a66cab44d0f73ed326579d09cd41ce0c3779d1
TakeshiMusgrave <takeshi.musgrave@mail.mcgill.ca>
2015-10-06 19:09:16 -0400

    LED alarm works when the temperature passes a threshold

--- Lab 2/source/main.c
+++ Lab 2/source/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include "system_init.h"
 
-#define D 10
+#define D 50
+#define E 45
 
 typedef struct{
 	float data[D];
@@ -14,28 +15,86 @@ typedef struct{
 uint32_t ticks;
 
 void move_motor(float temp){
-	//int delay=((Y-X)/(100/temp))+X;
+	int delay=((2300-900)*temp/100)+900; //values given in tutorial, we need to change them to suit our motor
 	GPIO_SetBits(GPIOB, GPIO_Pin_1);
 	
-	//for(int i=0;i<=delay;i++);
+	for(int i=0;i<=delay;i++);
 	GPIO_ResetBits(GPIOB, GPIO_Pin_1);
 
 }
 
-void prinf_buffer(){
-		
+
 	
-}
+	
 
 void update_buffer(float new_reading, buffer* moving_average){
-	float moving_average_sum=moving_average->sum;
-	int moving_average_index=moving_average->index;
-	int current_index=moving_average_index % D;
+	//float moving_average_sum=moving_average->sum;
+	//int moving_average_index=moving_average->index;
+	int current_index=moving_average->index % D;
 	float old_value=moving_average->data[current_index];
-	moving_average->sum=moving_average_sum-old_value;
+	moving_average->sum-=old_value;
 	moving_average->data[current_index]=new_reading;
-	moving_average->sum=moving_average->sum+new_reading;
-	moving_average->index=moving_average_index+1;
+	moving_average->sum+=new_reading;
+	moving_average->index++;
+}
+
+void update_buffer_test()
+{
+	buffer moving_average;
+	memset(&moving_average, 0, sizeof(moving_average));
+	
+	int listSize = D*2;
+	
+	for(int i=0;i<listSize;i++)
+	{
+		update_buffer((float)i,&moving_average);
+		
+		for(int j=0;i<D;i++)
+		{
+			printf("%f ", moving_average.data[j]);
+		}
+		printf("\n");
+		
+	}
+}
+
+void LED_alarm(float temp, int raw_counter)
+{
+	if (temp >= E){ // If temperature is above the emergency value
+		
+	int counter = raw_counter % 40; //turn on an led for 10 ticks. 4 LEDs means the counter should reset after 4*10 = 40.
+		
+	 // First time
+	 if (counter<10)
+	 {
+			GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+			GPIO_SetBits(GPIOD, GPIO_Pin_12);
+	 }
+		 // After 120 msec
+	 else if(counter<20)
+	 {
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+			GPIO_SetBits(GPIOD, GPIO_Pin_13);
+	 }
+	 
+	 else if(counter<30)
+	 {
+			GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+			GPIO_SetBits(GPIOD, GPIO_Pin_14);
+	 }
+	 	 
+	 else
+	 {
+			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+			GPIO_SetBits(GPIOD, GPIO_Pin_15);
+	 }
+	}
+	else{
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+	}
 }
 
 
@@ -61,14 +120,17 @@ int main(){
 	SysTick_Config(SystemCoreClock/50); //tick every 20 ms //Input value must be less than 24 bits
 	buffer moving_average;
 	memset(&moving_average, 0, sizeof(moving_average));
-	
+	int counter = 0;
 	while(1){
 		if(ticks){
 			//motor
 			//temp reading
 			//led control and alarm
+			counter++;
 			update_buffer(get_temp(),&moving_average);
-			printf("Temperature reading: %f, %f, %d\n", get_temp(), moving_average.sum, moving_average.index);
+			printf("Temperature reading: %f, %f, %d\n", get_temp(), moving_average.sum/D, moving_average.index);
+			LED_alarm(moving_average.sum/D,counter);
+			move_motor(moving_average.sum/D);
 			ticks=0;
 		}
 	}
